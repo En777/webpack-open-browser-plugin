7d4bf6f61d1cccd62bc97510b8878b165a3c9ada
En777 <tankdeeg@qq.com>
2021-08-20 23:13:13 +0800

    Open the browser should be once, fix

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ function WebpackOpenBrowserPlugin (options) {
   this.options = options || {}
 }
 
+function once (fn) {
+  var called = false
+  return function () {
+    if (called) return
+    called = true
+    fn()
+  }
+}
+
 WebpackOpenBrowserPlugin.prototype.apply = function (compiler) {
   var t = this
-  compiler.hooks.done.tap('WebpackOpenBrowserPlugin', function () {
+  // Open the browser should be once
+  compiler.hooks.done.tap('WebpackOpenBrowserPlugin', once(function () {
     var url = t.options.url
     if (!url) {
       console.error('please pass a url, like: new WebpackOpenBrowserPlugin({ url: "http://localhost:8080" })')
@@ -16,7 +26,7 @@ WebpackOpenBrowserPlugin.prototype.apply = function (compiler) {
       var open = require('opn')
       open(url)
     })
-  })
+  }))
 }
 
 module.exports = WebpackOpenBrowserPlugin
